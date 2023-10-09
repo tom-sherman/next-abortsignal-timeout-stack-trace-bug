2efd860dfe8559085d77483484b5dbd2cf72b52c
Tom Sherman <the.tomsherman@gmail.com>
2023-10-09 15:20:48 +0100

    Add reproduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+// Remove this to test stack trace in prerendering
+export const dynamic = "force-dynamic";
+
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+export default async function Home() {
+  await fetch("https://example.com", {
+    signal: AbortSignal.timeout(1),
+  });
+  return null;
 }
