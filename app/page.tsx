5916b7a99becf06007191e38f21d621c3b7616a4
Tom Sherman <the.tomsherman@gmail.com>
2023-10-09 15:24:51 +0100

    Tidy

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ export const dynamic = "force-dynamic";
 
 /** Add your relevant code here for the issue to reproduce */
 export default async function Home() {
+  // Can replace this signal with a native timeout signal for the same effect:
+  // `AbortSignal.timeout(1)`
+  const signal = makeTimeoutSignal(1);
+
   await fetch("https://example.com", {
-    signal: AbortSignal.timeout(1),
+    signal,
   });
   return null;
+}
+
+function makeTimeoutSignal(ms: number) {
+  const controller = new AbortController();
+  setTimeout(() => controller.abort(), ms);
+  return controller.signal;
 }
